6c5f7c981a6eeeb4d84648e7c19f3bacb9ed0b9b
RJ <103343995+RJPhenom@users.noreply.github.com>
2026-03-20 11:43:05 -0400

    setup for pages with github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rjphenom.github.io',
+  base: '/mmnb',
   vite: {
     plugins: [tailwindcss()]
   }
